32164997287da065c214fb44af9601a28444827e
Leonard <leonbrkanac@gmail.com>
2019-11-11 20:03:19 -0800

    Unit Tests

---
+++ app/tests/test_auth.py
@@ -0,0 +1,114 @@
+# app/auth/controllers.py
+import unittest
+import os, sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
+from flask import Flask, render_template, current_app, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from app.auth.controllers import auth
+from app.auth.models import User
+from flask_login import login_user
+from app import db, login_manager
+
+from app.api.controllers import api
+from app.auth.controllers import auth
+from app.site.controllers import site
+from app.profiles.controllers import profiles
+
+from app import app
+import config
+
+class APITests(unittest.TestCase):
+    def create_app(self):
+
+        app = Flask('Testing')
+        app.config.from_object('config') 
+        app.jinja_env.cache = {} # https://blog.socratic.org/the-one-weird-trick-that-cut-our-flask-page-load-time-by-70-87145335f679?gi=a8f7d3ec3e71
+
+        db = SQLAlchemy(app)
+
+        login_manager = LoginManager()
+        login_manager.init_app(app)
+        app.secret_key = config.login_manager_secret_key
+
+        @app.errorhandler(404)
+        def not_found(e):
+            return render_template('404.html'), 404
+        app.register_blueprint(api)
+        app.register_blueprint(auth)
+        app.register_blueprint(site)
+        app.register_blueprint(profiles)
+
+        db.create_all()
+
+        
+        return app
+        
+    # executed prior to each test
+    def setUp(self):
+        #need to create a fake user to run tests on 
+        db.session.commit()
+        db.drop_all()
+        db.create_all()
+        app.config.from_object('config') 
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+
+        login_manager = LoginManager()
+        login_manager.init_app(app)
+        app.secret_key = config.login_manager_secret_key
+
+        app.register_blueprint(auth)
+
+        self.app = app.test_client()
+
+        self.user = User('marioguy4699@gmail,com','marioguy08','hello1234')
+        db.session.add(self.user)
+        db.session.commit()
+    # executed after each test
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    #Debugger : paste this in -> `import pdb; pdb.set_trace()`
+    def test_signup(self):
+        url = 'auth/signup'
+        response = self.app.get(url, follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_signin(self):
+        url = 'auth/signin'
+        response = self.app.get(url, follow_redirects=True)
+        #import pdb; pdb.set_trace();
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_post(self):
+        url = 'auth/signup'
+        response = self.app.post(url, follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_signin_post(self):
+        url = 'auth/signin'
+        response = self.app.post(url, follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_signout(self):
+        with app.app_context(), app.test_request_context():
+            url = 'auth/signout'
+            login_user(self.user, remember=False)
+            response = self.app.get(url, follow_redirects=True)
+            self.assertEqual(response.status_code, 401)
+
+    def test_user_count(self):
+        self.assertEqual(User.query.count(),1)
+
+    
+    def test_404_not_found(self):
+        response = self.app.get('/nothinghere')
+        self.assertEqual(response.status_code, 404)
+    
+    
+if __name__ == "__main__":
+    unittest.main()
---
+++ app/tests/test_profiles.py
@@ -0,0 +1,124 @@
+import unittest
+import os, sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
+from flask import Flask, render_template, current_app, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from app.auth.controllers import auth
+from app.auth.models import User
+from flask_login import login_user
+from app import db, login_manager
+
+from app.api.controllers import api
+from app.auth.controllers import auth
+from app.site.controllers import site
+from app.profiles.controllers import profiles
+
+from app import app
+import config
+
+class APITests(unittest.TestCase):
+    def create_app(self):
+
+        app = Flask('Testing')
+        app.config.from_object('config') 
+        app.jinja_env.cache = {} # https://blog.socratic.org/the-one-weird-trick-that-cut-our-flask-page-load-time-by-70-87145335f679?gi=a8f7d3ec3e71
+
+        db = SQLAlchemy(app)
+
+        login_manager = LoginManager()
+        login_manager.init_app(app)
+        app.secret_key = config.login_manager_secret_key
+
+        @app.errorhandler(404)
+        def not_found(e):
+            return render_template('404.html'), 404
+        app.register_blueprint(api)
+        app.register_blueprint(auth)
+        app.register_blueprint(site)
+        app.register_blueprint(profiles)
+
+        db.create_all()
+
+        
+        return app
+        
+    # executed prior to each test
+    def setUp(self):
+        #need to create a fake user to run tests on 
+        db.session.commit()
+        db.drop_all()
+        db.create_all()
+        app.config.from_object('config') 
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+
+        login_manager = LoginManager()
+        login_manager.init_app(app)
+        app.secret_key = config.login_manager_secret_key
+
+        app.register_blueprint(auth)
+
+        self.app = app.test_client()
+
+        self.user = User('marioguy4699@gmail,com','marioguy08','hello1234')
+        db.session.add(self.user)
+        db.session.commit()
+    # executed after each test
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_me(self):
+        with app.app_context(), app.test_request_context():
+            login_user(self.user, remember=False)
+            url = 'profile/me'
+            response = self.app.get(url, follow_redirects=True)
+            self.assertEqual(response.status_code,401)
+
+    def test_me_post(self):
+        with app.app_context(), app.test_request_context():
+            login_user(self.user, remember=False)
+            url = 'profile/me'
+            response = self.app.post(url, follow_redirects=True)
+            self.assertEqual(response.status_code,401)
+
+    def test_delete(self):
+        with app.app_context(), app.test_request_context():
+            login_user(self.user, remember=False)
+            url = 'profile/delete'
+            response = self.app.get(url, follow_redirects=True)
+            self.assertEqual(response.status_code,401)
+    
+    def test_delete_post(self):
+        with app.app_context(), app.test_request_context():
+            login_user(self.user, remember=False)
+            url = 'profile/delete'
+            response = self.app.post(url, follow_redirects=True)
+            self.assertEqual(response.status_code,401)
+
+    def test_search(self):
+        with app.app_context(), app.test_request_context():
+            login_user(self.user, remember=False)
+            url = 'profile/search'
+            response = self.app.get(url, follow_redirects=True)
+            self.assertEqual(response.status_code,401)
+
+    def test_search_post(self):
+        with app.app_context(), app.test_request_context():
+            login_user(self.user, remember=False)
+            url = 'profile/search'
+            response = self.app.post(url, follow_redirects=True)
+            self.assertEqual(response.status_code,401)
+
+    def test_user_page(self):
+        with app.app_context(), app.test_request_context():
+            login_user(self.user, remember=False)
+            url = 'profile/user/<id>'
+            response = self.app.get(url, follow_redirects=True)
+            self.assertEqual(response.status_code,401)
+
+if __name__ == "__main__":
+    unittest.main()
